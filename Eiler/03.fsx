e38a81567e35fa47990487ba167ec800a1446361
avanin <avanin@ecocompany.ru>
2016-10-19 11:58:51 +0300

    Problem 3

--- Eiler/03.fsx
+++ Eiler/03.fsx
@@ -1,17 +1,14 @@
 ﻿// The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143
 
-let isPrime = fun (n, x) -> x % n = 0
+let isPrime = fun (x:int64, n:int64) -> x % n = 0L
+let isBigger = fun (x:int64, n:int64) -> if x > n then x else n
 
-let allPrimes num =
-  let rec allPrimes' (n, x) =
-    seq { // sequences are lazy, so we can make them infinite
-      if isPrime (n, x) then
-        yield n
-      yield! allPrimes' ((n+1), x) // recursing
-    }
-  allPrimes'(2, num) // starting from 2
+let createList n:int64 = 
+    let rec allPrimes'(k:int64, i:int64, num:int64) =
+        match k with
+        | 1L -> num
+        | _ -> if isPrime (k, i) then (k/i, 2L, (num, i) |> isBigger) |> allPrimes' else (k, i+1L, num) |> allPrimes'     
+    (n, 2L, 0L) |> allPrimes'
 
-allPrimes 13195
-|> Seq.take 10 // only 20
-|> List.ofSeq // forces evaluation of first 20 items
+createList 600851475143L //val it : int64 = 6857L
