f64b3a1673862f721c3abd08b852f75a4e9975c9
jasket <Asket030486>
2016-10-24 00:26:54 +0300

    Pr 7

---
+++ Eiler/07.fsx
@@ -0,0 +1,10 @@
+﻿// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+// What is the 10 001st prime number?
+
+let isPrime = fun (x, n) -> x % n = 0
+let rec numbersFrom n = 
+  seq { yield n
+        yield! numbersFrom (n + 1) }
+
+
+numbersFrom 2 |> Seq.take 6
